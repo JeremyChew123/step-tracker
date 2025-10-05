52094ddef33878c5c4ed7af874ab9577c54c3767
JeremyChew123 <cwqjeremy@gmail.com>
2025-10-05 19:35:50 +0800

    created xcode project

---
+++ Step Tracker/Step_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Step_TrackerApp.swift
+//  Step Tracker
+//
+//  Created by Jeremy Chew on 5/10/25.
+//
+
+import SwiftUI
+
+@main
+struct Step_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
