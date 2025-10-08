834f3f0a4546392058af766f195e06931edcb0b1
JeremyChew123 <cwqjeremy@gmail.com>
2025-10-08 21:49:46 +0800

    step bar chart interactivity

--- Step Tracker/DashboardView.swift
+++ Step Tracker/DashboardView.swift
@@ -27,12 +27,20 @@ struct DashboardView: View {
     @AppStorage("hasSeenPermissionPriming") private var hasSeenPermissionPriming = false
     @State private var isShowingPermissionSheet = false
     @State private var selectedStat: HealthMetricContext = .steps
+    @State private var rawSelectedDate: Date?
     var isSteps: Bool { selectedStat == .steps}
     
     var avgStepCount: Double {
         guard !hkManager.stepData.isEmpty else { return 0 }
         let totalSteps = hkManager.stepData.reduce(0) { $0 + $1.value} //there is date and value. we only want the value
         return totalSteps / Double(hkManager.stepData.count)
+    }
+    
+    var selectedHealthMetric: HealthMetric? {
+        guard let rawSelectedDate else { return nil }
+        return hkManager.stepData.first {
+            Calendar.current.isDate(rawSelectedDate, inSameDayAs: $0.date)
+        }
     }
     
     var body: some View {
@@ -72,6 +80,15 @@ struct DashboardView: View {
                         .foregroundStyle(.secondary)
                         
                         Chart {
+                            if let selectedHealthMetric {
+                                RuleMark(x: .value("Selected Metric", selectedHealthMetric.date, unit: .day))
+                                    .foregroundStyle(Color.secondary.opacity(0.3))
+                                    .offset(y: -10)
+                                    .annotation(position: .top, spacing: 0, overflowResolution: .init(x: .fit(to: .chart) , y: .disabled)) {
+                                        annotationView
+                                    }
+                            }
+                            
                             RuleMark(y: .value("Averages", avgStepCount))
                                 .foregroundStyle(Color.secondary)
                                 .lineStyle(.init(lineWidth: 1, dash: [5])) //linestyle conforms to strokestyle which has a documentation which needs an init to utilise
@@ -81,9 +98,11 @@ struct DashboardView: View {
                                         y: .value("Steps", steps.value)
                                 )
                                 .foregroundStyle(.pink.gradient)
+                                .opacity(rawSelectedDate = nil || steps.date == selectedHealthMetric?.date ? 1:0.3)
                             }
                         }
                         .frame(height: 150)
+                        .chartXSelection(value: $rawSelectedDate.animation(.easeInOut))
                         .chartXAxis{
                             AxisMarks {value in
                                 AxisValueLabel(format: .dateTime.month(.defaultDigits).day())
@@ -140,6 +159,23 @@ struct DashboardView: View {
             })
         }
         .tint(isSteps ? .pink : .indigo)
+    }
+    
+    var annotationView: some View {
+        VStack(alignment: .leading) {
+            Text(selectedHealthMetric?.date ?? .now, format:
+                    .dateTime.weekday(.abbreviated).month(.abbreviated).day())
+            .font(.footnote.bold())
+            .foregroundStyle(.secondary)
+            
+            Text(selectedHealthMetric?.value ?? 0, format: .number.precision(.fractionLength(0)))
+                .fontWeight(.heavy)
+                .foregroundStyle(.pink)
+        }
+        .padding(12)
+        .background(RoundedRectangle(cornerRadius: 4)
+            .fill(Color(.secondarySystemBackground))
+            .shadow(color: .blue.opacity(0.3), radius: 2, x: 2, y: 2))
     }
 }
 
