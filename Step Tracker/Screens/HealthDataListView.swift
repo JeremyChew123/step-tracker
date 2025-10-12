f8bcbe6b3a914896345b9176982a5a1e0414c9c8
JeremyChew123 <cwqjeremy@gmail.com>
2025-10-12 21:52:22 +0800

    upload data to healthkit

--- Step Tracker/Screens/HealthDataListView.swift
+++ Step Tracker/Screens/HealthDataListView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 
 struct HealthDataListView: View {
     
-    var metric: HealthMetricContext
+    @Environment(HealthKitManager.self) private var hkManager
     @State private var isShowingAddDate = false
     @State private var addDataDate: Date = .now
     @State private var valueToAdd: String = ""
     
+    var metric: HealthMetricContext
+    var listData: [HealthMetric] {
+        metric == .steps ? hkManager.stepData : hkManager.weightData
+    }
+    
     var body: some View {
-        List(0..<28) {i in
+        List(listData.reversed()) {data in
             HStack {
-                Text(Date(), format: .dateTime.month().day().year())
+                Text(data.date, format: .dateTime.month().day().year())
                 Spacer()
-                Text(10000, format: .number.precision(.fractionLength(metric == .steps ? 0 : 1)))
+                Text(data.value, format: .number.precision(.fractionLength(metric == .steps ? 0 : 1)))
             }
         }
         .navigationTitle(metric.title)
@@ -50,7 +55,18 @@ struct HealthDataListView: View {
             .toolbar {
                 ToolbarItem(placement: .topBarTrailing) {
                     Button("Add Data") {
-                        //Add Code Later
+                        Task {
+                            if metric == .steps {
+                                await hkManager.addStepData(for: addDataDate, value: Double(valueToAdd)!)
+                                await hkManager.fetchStepCount()
+                                isShowingAddDate = false
+                            } else {
+                                await hkManager.addStepData(for: addDataDate, value: Double(valueToAdd)!)
+                                await hkManager.fetchWeights()
+                                await hkManager.fetchWeightsForDifferentials()
+                                isShowingAddDate = false
+                            }
+                        }
                     }
                 }
                 ToolbarItem(placement: .topBarLeading) {
@@ -66,5 +82,6 @@ struct HealthDataListView: View {
 #Preview {
     NavigationStack{
         HealthDataListView(metric: .steps)
+            .environment(HealthKitManager())
     }
 }
